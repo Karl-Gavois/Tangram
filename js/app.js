1dedf8835c40a87cf0d3c681a3f91b6157956ef8
Supramout <moutonmorgan@orange.fr>
2023-05-05 11:11:42 +0200

    final squarre

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const btnOne = document.getElementById("#btnOne");
+const btnTwo = document.getElementById("#btnTwo");
+const btwThree = document.getElementById("#btnThree");
+const btnFour = document.getElementById("#btnFour");
+const btnFive = document.getElementById("#btnFive");
+const btnSix = document.getElementById("#btnSix");
+const btnSeven = document.getElementById("#btnSeven");
+
